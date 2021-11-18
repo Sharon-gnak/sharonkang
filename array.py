8dfb96c1e6f168fa7d420ce6a3385b645c38ca7b
Sharon Kang <94573832+Sharon-gnak@users.noreply.github.com>
2021-11-18 00:49:02 -0500

    Create array.py

---
+++ array.py
@@ -0,0 +1,12 @@
+Write a function that takes an array of numbers and returns the sum of the numbers.
+The numbers can be negative or non-integer. 
+If the array does not contain any numbers then you should return 0.
+
+
+def sum_array(a):
+    if a == [] :
+        return 0
+    else :
+        return sum(a)
+      
+      #a 자체가 list
